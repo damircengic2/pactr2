7ee147446b21aa1391a29fa95bae72dc91d7988f
= <damir.cengic@stu.ssst.edu.ba>
2018-06-19 16:03:40 +0200

    Added sign pact screen, TouchID integration start, sign-pact buttons working

---
+++ Packr/Packr/PactSignViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PactSignViewController.swift
+//  Packr
+//
+//  Created by Damir Čengić on 19/06/2018.
+//  Copyright © 2018 mini1. All rights reserved.
+//
+
+import UIKit
+
+class PactSignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
